055be06febefc6f43dcacf90985e6bc94ec388d1
AsimaDzh <animasoon@gmail.com>
2025-10-19 16:17:41 +0600

    Added player flip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private Rigidbody2D rb2D;
 
+    private Rigidbody2D _rb2D;
 
     private void Awake()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        _rb2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        rb2D.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb2D.linearVelocity.y);
+        float _horizontalInput = Input.GetAxis("Horizontal");
+        _rb2D.linearVelocity = new Vector2(_horizontalInput * speed, _rb2D.linearVelocity.y);
         
+        //Jump
         if (Input.GetKey(KeyCode.Space))
-            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, speed);
+            _rb2D.linearVelocity = new Vector2(_rb2D.linearVelocity.x, speed);
+
+        //Flip
+        if (_horizontalInput > 0.01f)
+            transform.localScale = new Vector3(6, 6, 6);
+        else if (_horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-6, 6, 6);
     }
 }
