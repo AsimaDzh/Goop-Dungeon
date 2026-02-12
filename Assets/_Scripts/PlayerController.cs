d1712938c3256a6fc88d0c75987d1cf526e669a2
AsimaDzh <animasoon@gmail.com>
2026-02-12 23:26:35 +0600

    Added flip to movement

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -34,24 +34,19 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         _horizontal = context.ReadValue<Vector2>().x;
+        if (_horizontal > 0.01f)
+            transform.localScale = new Vector3(6, 6, 6);
+        else if (_horizontal < -0.01f)
+            transform.localScale = new Vector3(-6, 6, 6);
     }
     #endregion
 
     //private void Update()
     //{
-    //    float _horizontalInput = Input.GetAxis("Horizontal");
-    //    _rb2D.linearVelocity = new Vector2(
-    //        _horizontalInput * speed, 
-    //        _rb2D.linearVelocity.y);
 
     //    //Jump
     //    if (Input.GetKeyDown(KeyCode.Space))
     //        _rb2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
 
-    //    //Flip
-    //    if (_horizontalInput > 0.01f)
-    //        transform.localScale = new Vector3(6, 6, 6);
-    //    else if (_horizontalInput < -0.01f)
-    //        transform.localScale = new Vector3(-6, 6, 6);
     //}
 }
