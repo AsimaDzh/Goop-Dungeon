0c1f4101afad0f99ad389861a147b5340e921175
AsimaDzh <animasoon@gmail.com>
2026-02-16 13:22:45 +0600

    Edited PlayerController

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -42,29 +42,44 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rb2D = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _cachedQueryStartInCol = Physics2D.queriesStartInColliders;
+    }
+
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
     }
 
 
     private void FixedUpdate()
     {
-        _rb2D.linearVelocity = new Vector2(
-            _horizontal * speed,
-            _rb2D.linearVelocity.y);
+
     }
 
 
     #region PLAYER_CONTROLS
-    public void Move(InputAction.CallbackContext context)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        _horizontal = context.ReadValue<Vector2>().x;
-        if (_horizontal > 0.01f)
+        _frameInput = context.ReadValue<Vector2>().x;
+        
+        if (_frameInput > 0.01f)
             transform.localScale = new Vector3(6, 6, 6);
-        else if (_horizontal < -0.01f)
+        else if (_frameInput < -0.01f)
             transform.localScale = new Vector3(-6, 6, 6);
+
+        // Turns smooth input into hard one
+        if (stats.SnapInput)
+        {
+            if (Mathf.Abs(_frameInput.Move.x) < stats.HorizontalDeadZoneThreshold)
+                _frameInput.Move.x = 0;
+            else _frameInput.Move.x = Mathf.Sign(_frameInput.Move.x);
+        }
     }
 
 
-    public void Jump(InputAction.CallbackContext context)
+    public void OnJump(InputAction.CallbackContext context)
     {
         if(context.started && IsGrounded())
             _rb2D.linearVelocity = new Vector2(_rb2D.linearVelocity.x, jumpForce);
