241b68e453b8a087cbb438b5421398cd70ec9b57
AsimaDzh <animasoon@gmail.com>
2026-03-08 18:58:23 +0600

    Edit: Add forse when relese object

--- Assets/_Scripts/Player/GrabObjects.cs
+++ Assets/_Scripts/Player/GrabObjects.cs
@@ -5,15 +5,10 @@ public class GrabObjects : MonoBehaviour
     [SerializeField] private Transform grabPoint;
     [SerializeField] private Transform rayPoint;
     [SerializeField] private float rayDistance;
+    [SerializeField] private float throwForce;
+    [SerializeField] private LayerMask objectLayer;
 
     private GameObject _grabbedObject;
-    private int _layerIndex;
-
-
-    private void Start()
-    {
-        _layerIndex = LayerMask.NameToLayer("Objects");
-    }
 
 
     private void Update()
@@ -21,9 +16,10 @@ public class GrabObjects : MonoBehaviour
         RaycastHit2D _hitInfo = Physics2D.Raycast(
             rayPoint.position, 
             transform.right, 
-            rayDistance);
+            rayDistance,
+            objectLayer);
 
-        if (_hitInfo.collider != null && _hitInfo.collider.gameObject.layer == _layerIndex)
+        if (_hitInfo.collider != null)
         {
             if (_grabbedObject == null)
             {
@@ -37,10 +33,14 @@ public class GrabObjects : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E) && _grabbedObject != null)
         {
+            Rigidbody2D _rb2D = _grabbedObject.GetComponent<Rigidbody2D>();
+
             _grabbedObject.GetComponent<BoxCollider2D>().enabled = true;
-            _grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            _rb2D.isKinematic = false;
             _grabbedObject.transform.SetParent(null);
             _grabbedObject = null;
+
+            _rb2D.AddForce((transform.right + transform.up) * throwForce, ForceMode2D.Impulse);
         }
 
         Debug.DrawRay(rayPoint.position, transform.right * rayDistance, Color.red);
