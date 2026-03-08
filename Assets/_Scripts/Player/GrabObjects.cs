e0b1d63edeca53adde5c321251517c1fd1b6e9dc
AsimaDzh <animasoon@gmail.com>
2026-03-08 18:31:08 +0600

    Edit: Add release object in GrabObjects.cs

--- Assets/_Scripts/Player/GrabObjects.cs
+++ Assets/_Scripts/Player/GrabObjects.cs
@@ -25,7 +25,6 @@ public class GrabObjects : MonoBehaviour
 
         if (_hitInfo.collider != null && _hitInfo.collider.gameObject.layer == _layerIndex)
         {
-            // If the player is not already grabbing an object, grab the new one
             if (_grabbedObject == null)
             {
                 _grabbedObject = _hitInfo.collider.gameObject;
@@ -34,14 +33,14 @@ public class GrabObjects : MonoBehaviour
                 _grabbedObject.transform.position = grabPoint.position;
                 _grabbedObject.transform.SetParent(transform);
             }
+        }
 
-            if (Input.GetKeyDown(KeyCode.E) && _grabbedObject != null)
-            {
-                _grabbedObject.GetComponent<BoxCollider2D>().enabled = true;
-                _grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
-                _grabbedObject.transform.SetParent(null);
-                _grabbedObject = null;
-            }
+        if (Input.GetKeyDown(KeyCode.E) && _grabbedObject != null)
+        {
+            _grabbedObject.GetComponent<BoxCollider2D>().enabled = true;
+            _grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            _grabbedObject.transform.SetParent(null);
+            _grabbedObject = null;
         }
 
         Debug.DrawRay(rayPoint.position, transform.right * rayDistance, Color.red);
