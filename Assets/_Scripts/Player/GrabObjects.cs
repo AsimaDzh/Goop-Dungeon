cde85699107d3654803b2ed15269f0020f371fc9
AsimaDzh <animasoon@gmail.com>
2026-03-09 13:40:26 +0600

    Edit: Add ThrowObject method

--- Assets/_Scripts/Player/GrabObjects.cs
+++ Assets/_Scripts/Player/GrabObjects.cs
@@ -13,23 +13,12 @@ public class GrabObjects : MonoBehaviour
     private BoxCollider2D _grabbedCollider;
 
 
-
     private void Update()
     {
-        if (_grabbedObject == null) GrabObject();
-
-
-        if (Input.GetKeyDown(KeyCode.E) && _grabbedObject != null)
-        {
-            Rigidbody2D _rb2D = _grabbedObject.GetComponent<Rigidbody2D>();
-
-            _grabbedObject.GetComponent<BoxCollider2D>().enabled = true;
-            _rb2D.isKinematic = false;
-            _grabbedObject.transform.SetParent(null);
-            _grabbedObject = null;
-
-            _rb2D.AddForce((transform.right + transform.up) * throwForce, ForceMode2D.Impulse);
-        }
+        if (_grabbedObject == null) 
+            GrabObject();
+        else if (Input.GetKeyDown(KeyCode.E) && _grabbedObject != null)
+            ThrowObject();
 
         Debug.DrawRay(rayPoint.position, transform.right * rayDistance, Color.red);
     }
@@ -54,5 +43,18 @@ public class GrabObjects : MonoBehaviour
 
         _grabbedObject.transform.position = grabPoint.position;
         _grabbedObject.transform.SetParent(transform);
+    }
+
+
+    private void ThrowObject()
+    {
+        _grabbedCollider.enabled = true;
+        _grabbedRb.isKinematic = false;
+
+        _grabbedObject.transform.SetParent(null);
+
+        _grabbedRb.AddForce((transform.right + transform.up) * throwForce, ForceMode2D.Impulse);
+
+        _grabbedObject = null;
     }
 }
