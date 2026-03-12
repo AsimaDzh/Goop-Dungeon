09c15a18696a72bdead12ef0480dcd6e0c9f4695
AsimaDzh <animasoon@gmail.com>
2026-03-12 22:07:20 +0600

    Edit: Add events and Instance

--- Assets/_Scripts/Core/EventBus.cs
+++ Assets/_Scripts/Core/EventBus.cs
@@ -1,6 +1,33 @@
+using System;
 using UnityEngine;
 
 public class EventBus : MonoBehaviour
 {
+    public static EventBus Instance { get; private set; }
+    public event Action OnGamePaused;
+    public event Action OnGameResumed;
 
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    public void RaiseGamePaused()
+    {
+        OnGamePaused?.Invoke();
+    }
+
+
+    public void RaiseGameResumed()
+    {
+        OnGameResumed?.Invoke();
+    }
 }
+
