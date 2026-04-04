d6c79cf7d8f4d24f0f49dd2ca0ec4d79ef836322
Lemonka <animasoon@gmail.com>
2026-04-04 17:33:52 +0500

    Edit: Add methods and variables to PauseController.cs

--- Assets/_Scripts/UI/PauseController.cs
+++ Assets/_Scripts/UI/PauseController.cs
@@ -1,6 +1,90 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseController : MonoBehaviour
 {
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private Button buttonResume;
+    [SerializeField] private Button buttonMainMenu;
 
+
+    private void OnEnable()
+    {
+        if (EventBus.Instance != null)
+        {
+            EventBus.Instance.OnGamePaused += ShowPausePanel;
+            EventBus.Instance.OnGameResumed += HidePausePanel;
+        }
+        if (InputManager.Instance != null)
+        {
+            InputManager.Instance.OnPausePressed += HandlePausePressed;
+            InputManager.Instance.OnCancelPressed += HandleCancelPressed;
+        }
+    }
+
+
+    private void OnDisable()
+    {
+        if (EventBus.Instance != null)
+        {
+            EventBus.Instance.OnGamePaused -= ShowPausePanel;
+            EventBus.Instance.OnGameResumed -= HidePausePanel;
+        }
+        if (InputManager.Instance != null)
+        {
+            InputManager.Instance.OnPausePressed -= HandlePausePressed;
+            InputManager.Instance.OnCancelPressed -= HandleCancelPressed;
+        }
+    }
+
+
+    private void Start()
+    {
+        if (buttonResume != null)
+            buttonResume.onClick.AddListener(OnResumeClicked);
+        if (buttonMainMenu != null)
+            buttonMainMenu.onClick.AddListener(OnMainMenuClicked);
+    }
+
+
+    private void ShowPausePanel()
+    {
+        if (pausePanel != null)
+            pausePanel.SetActive(true);
+    }
+
+
+    private void HidePausePanel()
+    {
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+    }
+
+
+    private void OnResumeClicked()
+    {
+        if (GameManager.Instance != null)
+            GameManager.Instance.Resume();
+    }
+
+
+    private void OnMainMenuClicked()
+    {
+        if (GameManager.Instance != null)
+            GameManager.Instance.GoToMenu();
+    }
+
+
+    private void HandlePausePressed()
+    {
+        if (GameManager.Instance != null && GameManager.Instance.CurrentState == GameState.Playing)
+            GameManager.Instance.Pause();
+    }
+
+
+    private void HandleCancelPressed()
+    {
+        if (GameManager.Instance != null && GameManager.Instance.CurrentState == GameState.Paused)
+            GameManager.Instance.Resume();
+    }
 }
