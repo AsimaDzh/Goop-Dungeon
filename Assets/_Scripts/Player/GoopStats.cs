315f81010aff44f4c419ed96c5c79a8cbc59f9fa
AsimaDzh <animasoon@gmail.com>
2026-04-10 00:57:20 +0600

    Edit: Add stats to GoopStats.cs

--- Assets/_Scripts/Player/GoopStats.cs
+++ Assets/_Scripts/Player/GoopStats.cs
@@ -1,6 +1,21 @@
+using System;
 using UnityEngine;
 
 public class GoopStats : MonoBehaviour
 {
+    [Header("========== Player Data ===========")]
+    public GoopData playerData;
 
+    [Header("========== Current Stats (Runtime) ==========")]
+    [SerializeField] private float currentHealth;
+
+    [Tooltip("Current mana or player energy")]
+    [SerializeField] private float currentMana;
+
+    public float CurrentHealth => currentHealth;
+    public float CurrentMana => currentMana;
+
+    public event Action<float, float> OnHealthChanged;
+    public event Action<float, float> OnManaChanged;
+    public event Action OnDeath;
 }
