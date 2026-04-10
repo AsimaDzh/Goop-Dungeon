79ec9660b13b074c687922bad774b2bdff511f21
AsimaDzh <animasoon@gmail.com>
2026-04-11 00:14:25 +0600

    Edit: Create methods for GoopStats.cs

--- Assets/_Scripts/Player/GoopStats.cs
+++ Assets/_Scripts/Player/GoopStats.cs
@@ -9,13 +9,40 @@ public class GoopStats : MonoBehaviour
     [Header("========== Current Stats (Runtime) ==========")]
     [SerializeField] private float currentHealth;
 
-    [Tooltip("Current mana or player energy")]
-    [SerializeField] private float currentMana;
-
     public float CurrentHealth => currentHealth;
-    public float CurrentMana => currentMana;
 
     public event Action<float, float> OnHealthChanged;
-    public event Action<float, float> OnManaChanged;
     public event Action OnDeath;
+
+
+    private void Awake()
+    {
+        InitializeFromData();
+    }
+
+
+    public void InitializeFromData()
+    {
+        if (playerData == null)
+        {
+            Debug.LogError("PlayerStats: PlayerData is null!", this);
+            return;
+        }
+        currentHealth = Mathf.Clamp(playerData.MaxHealth, 1f, float.MaxValue);
+
+        OnHealthChanged?.Invoke(currentHealth, playerData.MaxHealth);
+    }
+
+
+    public void TakeDamage(float amount)
+    {
+        if (amount <= 0f || currentHealth <= 0f) return;
+
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, playerData.MaxHealth);
+
+        OnHealthChanged?.Invoke(currentHealth, playerData.MaxHealth);
+
+        if (currentHealth <= 0f) OnDeath?.Invoke();
+    }
 }
