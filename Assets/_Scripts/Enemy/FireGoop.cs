b03892f8d840109ca54fd5149824cb81fc680694
Lemonka <animasoon@gmail.com>
2026-04-18 18:09:31 +0500

    Edit: Add more vars in FireGoop.cs

--- Assets/_Scripts/Enemy/FireGoop.cs
+++ Assets/_Scripts/Enemy/FireGoop.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class FireGoop : EnemyBase
 {
-
+    [Header("========== Range Attack ==========")]
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform shootOrigin;
+    [SerializeField] private LayerMask projectileHitLayers;
+    [SerializeField] private float projectileSpeed = 12f;
+    [SerializeField] private float projectileDistance = 12f;
+    [SerializeField] private float aimOffsetY = 1f;
 }
