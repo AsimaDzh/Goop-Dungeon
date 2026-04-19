a946666f9e9d4658197276248d4dbbeb2c483de6
Lemonka <animasoon@gmail.com>
2026-04-19 16:43:23 +0500

    Edit: Add vars to the FireBall.cs

--- Assets/_Scripts/Projectiles/FireBall.cs
+++ Assets/_Scripts/Projectiles/FireBall.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
-
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float maxDistance = 20f;
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private LayerMask hitLayers;
 }
