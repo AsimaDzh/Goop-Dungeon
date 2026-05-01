f64962411acd0cc7ea32a25df2f0bf44de170f99
AsimaDzh <animasoon@gmail.com>
2026-05-01 16:12:44 +0600

    Edit: Make plan and add empty methods

--- Assets/_Scripts/NPC/NPCBase.cs
+++ Assets/_Scripts/NPC/NPCBase.cs
@@ -2,5 +2,47 @@ using UnityEngine;
 
 public class NPCBase : MonoBehaviour
 {
+    // Variables
 
+    private void Update()
+    {
+        // switch case
+        // state idle
+        // state moving
+        // state interacting
+        // state Inspecting
+        // state Accepting
+        // state Rejecting
+    }
+
+
+    private void HandleIdle()
+    {
+    }
+
+
+    private void HandleMoving()
+    {
+    }
+
+
+    private void GrabObject()
+    {
+    }
+
+
+    private void InspectiongObject()
+    {
+
+    }
+
+
+    private void AcceptingObject()
+    {
+    }
+
+
+    private void RejectingObject()
+    {
+    }
 }
