adb5d73d4ba1e88d4c2e0b09223bb71236c94545
Lemonka <animasoon@gmail.com>
2026-05-16 18:11:40 +0500

    Add: Create Start and Update methods fot BubbleShieldTimer.cs

--- Assets/_Scripts/UI/BubbleShieldTimer.cs
+++ Assets/_Scripts/UI/BubbleShieldTimer.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,4 +7,27 @@ public class BubbleShieldTimer : MonoBehaviour
     [SerializeField] private WaterGoop waterGoop;
     [SerializeField] private float cooldownTime; 
     private Image _bubbleImage;
+    private bool _isCooldown;
+
+
+    private void Start()
+    {
+        _bubbleImage = GetComponent<Image>();
+        _isCooldown = true;
+    }
+
+
+    private void Update()
+    {
+        if (_isCooldown)
+        {
+            _bubbleImage.fillAmount -= 1 / cooldownTime * Time.deltaTime;
+            if (_bubbleImage.fillAmount <= 0)
+            {
+                _bubbleImage.fillAmount = 1;
+                _isCooldown = false;
+                waterGoop.Timer.SetActive(false);
+            }
+        }
+    }
 }
