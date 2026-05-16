94a21861a9489f14d757edcaffd196363d0bb14e
Lemonka <animasoon@gmail.com>
2026-05-16 18:04:18 +0500

    Edit: Add vars in BubbleShieldTimer.cs

--- Assets/_Scripts/UI/BubbleShieldTimer.cs
+++ Assets/_Scripts/UI/BubbleShieldTimer.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BubbleShieldTimer : MonoBehaviour
 {
-
+    [SerializeField] private WaterGoop waterGoop;
+    [SerializeField] private float cooldownTime; 
+    private Image _bubbleImage;
 }
