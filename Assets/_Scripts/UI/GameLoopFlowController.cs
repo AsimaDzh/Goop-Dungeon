a06dfee1e502ebb8917678a684c98373e7747dfe
Asima <animasoon@gmail.com>
2026-05-19 13:03:31 +0500

    Add: Create vars for GameLoopFlowController.cs

--- Assets/_Scripts/UI/GameLoopFlowController.cs
+++ Assets/_Scripts/UI/GameLoopFlowController.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLoopFlowController : MonoBehaviour
 {
+    [Header("========== GameOver Panel =========")]
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private Button tryAgainButton;
+    [SerializeField] private Button menuButton;
 
+    [Header("========== Win Panel ==========")]
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private Button continueButton;
+
+    [Header("========== Shared UI ==========")]
+    [SerializeField] private GameObject pauseMenu;
+
+    [Header("========== Win Condition ==========")]
+    [SerializeField] private GameObject exitActivationObjectOverride;
+
+    private GoopStats _playerStats;
+    private bool _flowFinished;
 }
