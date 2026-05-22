61cd95d7c975c4eb9148c13ae42f02ac31b8d3c9
AsimaDzh <animasoon@gmail.com>
2026-05-22 23:56:48 +0500

    Add: Create methods for ExitDoorOpen.cs and edit prefabs

--- Assets/_Scripts/UI/ExitDoorOpen.cs
+++ Assets/_Scripts/UI/ExitDoorOpen.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class ExitDoorOpen : MonoBehaviour
 {
+    [SerializeField] private GameObject exit;
+    [SerializeField] private GrabObjects grabObjects;
 
+    private void Reset()
+    {
+        if (exit == null)
+            exit = FindFirstObjectByType<ExitWinTrigger>().gameObject;
+        if (grabObjects == null)
+            grabObjects = FindFirstObjectByType<GrabObjects>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Key")) return;
+
+        exit.SetActive(true);
+        gameObject.SetActive(false);
+        grabObjects.RemoveObject();
+    }
 }
