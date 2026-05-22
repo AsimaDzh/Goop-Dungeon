4693dc47060967a50f1f5be6bc82046e8304bdb8
AsimaDzh <animasoon@gmail.com>
2026-05-22 20:32:35 +0500

    Add: Create Key name in the ItemsNames.cs and edit exit door prefab

--- Assets/_Scripts/Core/ItemsNames.cs
+++ Assets/_Scripts/Core/ItemsNames.cs
@@ -1,5 +1,6 @@
 public static class ItemsNames
 {
+    public const string Key = "Key";
     public const string Rock = "Rock";
     public const string Acorn = "Acorn";
     public const string Flower = "Flower";
