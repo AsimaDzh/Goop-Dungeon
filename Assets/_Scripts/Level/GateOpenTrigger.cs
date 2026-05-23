698d4cc320f68c2a6a8016f10c3505647650effc
Lemonka <animasoon@gmail.com>
2026-05-23 16:58:06 +0500

    Edit: Add var, Awake method in GateOpenTrigger.cs and edit prefabs

--- Assets/_Scripts/Level/GateOpenTrigger.cs
+++ Assets/_Scripts/Level/GateOpenTrigger.cs
@@ -3,5 +3,12 @@ using UnityEngine;
 
 public class GateOpenTrigger : MonoBehaviour
 {
+    [SerializeField] private GameObject gateToOpen;
 
+
+    private void Awake()
+    {
+        if (gateToOpen == null)
+            gateToOpen = GameObject.FindGameObjectWithTag("Gate");
+    }
 }
