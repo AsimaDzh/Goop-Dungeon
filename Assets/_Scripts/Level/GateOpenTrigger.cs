95cd5815b33a636fa5622d58615b6de463e0e63f
Lemonka <animasoon@gmail.com>
2026-05-23 17:05:09 +0500

    Add: Create method OnTriggerEnter2D in GateOpenTrigger.cs

--- Assets/_Scripts/Level/GateOpenTrigger.cs
+++ Assets/_Scripts/Level/GateOpenTrigger.cs
@@ -5,10 +5,22 @@ public class GateOpenTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject gateToOpen;
 
+    private LayerMask ObjectMask => LayerMask.GetMask("Objects");
+
 
     private void Awake()
     {
         if (gateToOpen == null)
             gateToOpen = GameObject.FindGameObjectWithTag("Gate");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == ObjectMask)
+        {
+            gateToOpen.SetActive(false);
+            Destroy(gameObject);
+        }
     }
 }
