ce06ace5ee40fe4b509276eb003492329f6e9e80
Lemonka <animasoon@gmail.com>
2026-05-23 17:56:34 +0500

    Edit: Add vars and methods

--- Assets/_Scripts/Level/Lava.cs
+++ Assets/_Scripts/Level/Lava.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float damageInterval = 2f;
+    private IDamageable _playerDamageable;
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        _playerDamageable = collision.GetComponent<IDamageable>();
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (_playerDamageable == null || _playerDamageable.IsDead) return;
+
+        _playerDamageable.TakeDamage(damage);
+    }
 }
