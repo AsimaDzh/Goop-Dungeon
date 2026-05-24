0d88454a5a4849989be50bc6d473b20cece4cdb0
Lemonka <animasoon@gmail.com>
2026-05-24 16:54:05 +0500

    Edit: Add vars for Teleport.cs

--- Assets/_Scripts/Level/Teleport.cs
+++ Assets/_Scripts/Level/Teleport.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
+    [SerializeField] private Transform destination;
 
+    private HashSet<GameObject> _telepotObjects = new HashSet<GameObject>();
 }
