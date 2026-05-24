6ee0325a6f86ccf5c486278585129ff46b1e767e
Lemonka <animasoon@gmail.com>
2026-05-24 17:00:58 +0500

    Edit: Add OnTriggerEnter2D method

--- Assets/_Scripts/Level/Teleport.cs
+++ Assets/_Scripts/Level/Teleport.cs
@@ -6,4 +6,15 @@ public class Teleport : MonoBehaviour
     [SerializeField] private Transform destination;
 
     private HashSet<GameObject> _telepotObjects = new HashSet<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isActiveAndEnabled || !gameObject.activeInHierarchy) return;
+        if (_telepotObjects.Contains(collision.gameObject)) return;
+
+        if (destination.TryGetComponent(out Teleport destinationTeleport))
+            destinationTeleport._telepotObjects.Add(collision.gameObject);
+
+        collision.transform.position = destination.position;
+    }
 }
