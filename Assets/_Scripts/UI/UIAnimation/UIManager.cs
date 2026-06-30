f9910609977782aa901f169025d8d217eb1c891d
AsimaDzh <animasoon@gmail.com>
2026-06-30 18:12:20 +0500

    Edit: Add vars to UIManager.cs

--- Assets/_Scripts/UI/UIAnimation/UIManager.cs
+++ Assets/_Scripts/UI/UIAnimation/UIManager.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup
+        titleFade, 
+        playButton, 
+        optionsButton, 
+        extrasButton, 
+        leaveButton;
 
+    private float _fadeDurTitle = 2f;
+    private float _fadeDurButtons = 1f;
+    private float _fadeDurPlayOptionsCreditsLeave = 1f;
+    private float _delayBetweenButtons = 0.1f;
 }
