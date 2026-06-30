a853cf3dbf08542a9f52a153754234fc22061e22
AsimaDzh <animasoon@gmail.com>
2026-06-30 18:31:17 +0500

    Edit: Edit main menu scene and vars

--- Assets/_Scripts/UI/UIAnimation/UIManager.cs
+++ Assets/_Scripts/UI/UIAnimation/UIManager.cs
@@ -11,7 +11,14 @@ public class UIManager : MonoBehaviour
         leaveButton;
 
     private float _fadeDurTitle = 2f;
-    private float _fadeDurButtons = 1f;
-    private float _fadeDurPlayOptionsCreditsLeave = 1f;
+    private float _fadeDurAllButtons = 1f;
+    private float _fadeDurForEachButton = 1f;
     private float _delayBetweenButtons = 0.1f;
+
+    private Tween 
+        _animTitle, 
+        _animPlayButton, 
+        _animOptionsButton, 
+        _animExtrasButton, 
+        _animLeaveButton;
 }
