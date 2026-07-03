34f87fc4997d8f3f920da2192bba9cec0435a9e8
AsimaDzh <animasoon@gmail.com>
2026-07-03 19:56:40 +0500

    Edit: Edit vars again and add new animations to UIManager.cs

--- Assets/_Scripts/UI/UIAnimation/UIManager.cs
+++ Assets/_Scripts/UI/UIAnimation/UIManager.cs
@@ -3,21 +3,19 @@ using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup
-        titleFade, 
-        playButton, 
-        optionsButton, 
-        extrasButton, 
-        leaveButton;
-
-    private float _titleDuration = 0.7f;
-    private float _buttonsDuration = 0.3f;
-
+    [SerializeField] private CanvasGroup title, playButton, optionsButton, extrasButton, leaveButton;
+    [SerializeField] private RectTransform allButtons;
+    
+    private float _titleDur = 1f;
+    private float _titleFadeDur = 0.5f;
+    private float _buttonsDur = 2f;
+    private float _buttonsFadeDur = 0.3f;
     private Sequence _menuSequence;
 
 
     private void Awake()
     {
+        title.alpha = 0f;
         playButton.alpha = 0f;
         optionsButton.alpha = 0f;
         extrasButton.alpha = 0f;
@@ -33,14 +31,17 @@ public class UIManager : MonoBehaviour
 
     private void ShowMenuButtons()
     {
+        title.DOFade(1f, _titleFadeDur);
+        allButtons.DOAnchorPosY(0, _buttonsDur).From(new Vector2(0, 100)).SetEase(Ease.OutBack);
+
         _menuSequence = DOTween.Sequence();
 
         _menuSequence
-            .Append(titleFade.transform.DOScale(1, _titleDuration).From(5).SetEase(Ease.OutBack))
-            .Append(playButton.DOFade(1f, _buttonsDuration))
-            .Append(optionsButton.DOFade(1f, _buttonsDuration))
-            .Append(extrasButton.DOFade(1f, _buttonsDuration))
-            .Append(leaveButton.DOFade(1f, _buttonsDuration));
+            .Append(title.transform.DOScale(1, _titleDur).From(5).SetEase(Ease.OutBack))
+            .Append(playButton.DOFade(1f, _buttonsFadeDur))
+            .Append(optionsButton.DOFade(1f, _buttonsFadeDur))
+            .Append(extrasButton.DOFade(1f, _buttonsFadeDur))
+            .Append(leaveButton.DOFade(1f, _buttonsFadeDur));
     }
 
 
