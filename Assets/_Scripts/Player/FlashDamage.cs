c0c5c57043875cfde2d6d2a03992a4080724f27f
AsimaDzh <animasoon@gmail.com>
2026-07-09 13:48:20 +0500

    Edit: Add vars to the FlashDamage.cs

--- Assets/_Scripts/Player/FlashDamage.cs
+++ Assets/_Scripts/Player/FlashDamage.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class FlashDamage : MonoBehaviour
 {
+    [SerializeField] private float flashDelay = 1f;
 
+    private SpriteRenderer[] _spriteRenderers;
+    private MaterialPropertyBlock _propertyBlock;
+    private float _flashFactor;
 }
