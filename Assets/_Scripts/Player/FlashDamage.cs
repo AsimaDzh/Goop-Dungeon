4f2ed0352455e16d04b54641e3cd734fc11d8c2d
AsimaDzh <animasoon@gmail.com>
2026-07-09 13:57:41 +0500

    Edit: Create ApplyFlashFactor() and Flash() in FlashDamage.cs

--- Assets/_Scripts/Player/FlashDamage.cs
+++ Assets/_Scripts/Player/FlashDamage.cs
@@ -4,7 +4,29 @@ public class FlashDamage : MonoBehaviour
 {
     [SerializeField] private float flashDelay = 1f;
 
-    private SpriteRenderer[] _spriteRenderers;
+    private SpriteRenderer _spriteRenderer;
     private MaterialPropertyBlock _propertyBlock;
     private float _flashFactor;
+
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _propertyBlock = new MaterialPropertyBlock();
+    }
+
+
+    public void Flash()
+    {
+        _flashFactor = 1f;
+        ApplyFlashFactor();
+    }
+
+
+    private void ApplyFlashFactor()
+    {
+        _spriteRenderer.GetPropertyBlock(_propertyBlock);
+        _propertyBlock.SetFloat("_FlashFactor", _flashFactor);
+        _spriteRenderer.SetPropertyBlock(_propertyBlock);
+    }
 }
