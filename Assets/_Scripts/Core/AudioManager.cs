fb159e683645e3b603c50a2f298f0768836dff8f
AsimaDzh <animasoon@gmail.com>
2026-07-12 20:00:09 +0500

    Edit: Add OnEnable() to authomatically name lists

--- Assets/_Scripts/Core/AudioManager.cs
+++ Assets/_Scripts/Core/AudioManager.cs
@@ -3,25 +3,17 @@ using System;
 
 public enum AudioType
 {
-    //Interactables
-    DoorLock = 0,
-    GateButton = 1,
-    LavaHit = 2,
+    Interactables = 0,
 
     //Skills
-    BubbleShield = 3,
-    FireballLaunch = 4,
-    FireballHit = 5,
-    ThrowItem = 6,
+    BubbleShield = 1,
+    Fireball = 2,
+    ThrowItem = 3,
 
-    //UI
-    ButtonSelect = 7,
-    GameOver = 8,
-    GamePause = 9,
-    GameWin = 10
+    UI = 4,
 }
 
-[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioSource)), ExecuteInEditMode]
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
@@ -53,12 +45,25 @@ public class AudioManager : MonoBehaviour
     {
         //Instance._audioSource.PlayOneShot(Instance.soundList[(int)_audio], _volume);
     }
+
+
+#if UNITY_EDITOR
+    private void OnEnable()
+    {
+        string[] _names = Enum.GetNames(typeof(AudioType));
+        Array.Resize(ref soundList, _names.Length);
+
+        for (int i = 0; i < soundList.Length; i++)
+            soundList[i].name = _names[i];
+    }
+#endif
 }
 
 
 [Serializable]
 public struct AudioList
 {
+    [HideInInspector] public string name;
     [SerializeField] private AudioClip[] sounds;
 }
 
