6f8b549a7fb8e43f505a38b4f7998203f27002cd
AsimaDzh <animasoon@gmail.com>
2026-07-15 14:29:56 +0500

    Edit: Add methods to GameUIManager.cs

--- Assets/_Scripts/UI/UIAnimation/GameUIManager.cs
+++ Assets/_Scripts/UI/UIAnimation/GameUIManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class GameUIManager : MonoBehaviour
 {
@@ -6,11 +7,61 @@ public class GameUIManager : MonoBehaviour
     [SerializeField] private CanvasGroup gameOverBackground;
     [SerializeField] private CanvasGroup gameOverText;
     [SerializeField] private CanvasGroup gameOverButtons;
+
     private float _gameOverDur = 1f;
+    private Sequence _gameOverSequence;
 
     [Header("========== Win UI ==========")]
     [SerializeField] private CanvasGroup winBackground;
     [SerializeField] private CanvasGroup winText;
     [SerializeField] private CanvasGroup menuButton;
+
     private float _winDur = 0.5f;
+    private Sequence _winSequence;
+
+
+    private void Awake()
+    {
+        gameOverBackground.alpha = 0f;
+        gameOverText.alpha = 0f;
+        gameOverButtons.alpha = 0f;
+        winBackground.alpha = 0f;
+        winText.alpha = 0f;
+        menuButton.alpha = 0f;
+    }
+
+
+    public void ShowGameOverUI()
+    {
+       _gameOverSequence = DOTween.Sequence();
+
+       _gameOverSequence
+           .Append(gameOverBackground.DOFade(1f, _gameOverDur))
+           .Append(gameOverText.DOFade(1f, _gameOverDur))
+           .Append(gameOverButtons.DOFade(1f, _gameOverDur));
+    }
+
+
+    public void ShowWinUI()
+    {
+        _winSequence = DOTween.Sequence();
+
+        _winSequence
+            .Append(winBackground.DOFade(1f, _winDur))
+            .Append(winText.DOFade(1f, _winDur))
+            .Append(menuButton.DOFade(1f, _winDur));
+    }
+
+
+    private void OnDestroy()
+    {
+        ClearAnimations();
+    }
+
+
+    private void ClearAnimations()
+    {
+        _gameOverSequence?.Kill();
+        _winSequence?.Kill();
+    }
 }
