819c4dbf0357350a56e3345e7b9da284c2698bdd
AsimaDzh <animasoon@gmail.com>
2026-07-15 14:18:40 +0500

    Edit: Add vars to GameUIManager.cs

--- Assets/_Scripts/UI/UIAnimation/GameUIManager.cs
+++ Assets/_Scripts/UI/UIAnimation/GameUIManager.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class GameUIManager : MonoBehaviour
 {
+    [Header("========== GameOver UI ==========")]
+    [SerializeField] private CanvasGroup gameOverBackground;
+    [SerializeField] private CanvasGroup gameOverText;
+    [SerializeField] private CanvasGroup gameOverButtons;
+    private float _gameOverDur = 1f;
 
+    [Header("========== Win UI ==========")]
+    [SerializeField] private CanvasGroup winBackground;
+    [SerializeField] private CanvasGroup winText;
+    [SerializeField] private CanvasGroup menuButton;
+    private float _winDur = 0.5f;
 }
